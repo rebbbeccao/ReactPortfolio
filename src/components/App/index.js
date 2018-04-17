99fdb08c8df9486e45ba7338f0dda68af66287fc
Rebecca A. Olson <rebbbeccao@gmail.com>
2018-04-16 17:59:04 -0700

    working on header component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -19,6 +19,7 @@ class App extends Component {
         <Experience />
         <Contact />
         <Tech />
+
         <Footer />
       </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = props => {
-  return <div>Header component</div>;
+  return (
+    <div className="container navbar">
+      <div className="row">
+        <div className="col-sm-3">
+          <a>About</a>
+        </div>
+        <div className="col-sm-3">
+          <a>Contact</a>
+        </div>
+        <div className="col-sm-3">
+          <a>Experience</a>
+        </div>
+        <div className="col-sm-3">
+          <a>News</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
