d2bedf253cbb5906c68562912d26e15071cb87b5
Rebecca A. Olson <rebbbeccao@gmail.com>
2018-05-08 18:54:21 -0700

    added description

--- src/components/Experience/Card/CardFront.js
+++ src/components/Experience/Card/CardFront.js
@@ -9,11 +9,11 @@ export default class CardFront extends Component {
     return (
       <div className="card-side side-front side-front-content">
         <div className="container-fluid">
-          <div className="row project-name">
-            <h2>{this.props.name}</h2>
-          </div>
           <div className="row">
             <img src={this.props.projectImage} />
+          </div>
+          <div className="row project-name">
+            <h2>{this.props.name}</h2>
           </div>
         </div>
       </div>
