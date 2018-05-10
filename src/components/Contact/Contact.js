46fac18be3b7908de05659d9b14f78ce78a64bf4
Rebecca A. Olson <rebbbeccao@gmail.com>
2018-05-09 17:56:43 -0700

    contact styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,67 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './Contact.css';
 
-const Contact = props => {
-  return (
-    <div className="contact-container">
-      <div className="container contact-content">
-        <div className="row">
-          <h1 className="contact-title">Let's Connect!</h1>
-        </div>
-        <div className="row contact-row">
-          <div className="col-sm-6 column">
-            <div className="me-pic">
-              <img
-                src="https://lh3.googleusercontent.com/q4RmFnhkbD1uTfkieAhxGWJGLnyikDxKNwvdWSGr-c0gZTDSlBrGQAHUlIGwd4ytkiibuiJZ2XIohk-iOh6XsICUGm5AxzxYwxyQ-eL7pjPmkhAA98OXjMI_OYcE4iQZCvh5FfzkW9AZ3xnH26ebv9Eb2hf9c3QcEqzmkgP_bW1Tue6WzouVkeeOXV5bLZ9zgRqm9WONjP-BsGTZ3OOUjzzSScElcOMInLhq4C3TD50VyDUOhA_KDDiQnaoOoNJzY4q9R5HtrVrlTSLT0u65XZMX3BiAlgDp3UWZoOyeNPPbufUeu4oLr7_I5tB8Ls5MUI6EVKz2ysepAuK6W2ericGbaQMz9zwfr6REurkE84llioxXx1DrcC55qK4ncJ-KUYJ3hR9U_w1OnaQbqDwrD3iMDgTs07Jo6edcZ40Tu9pW1howCES7DWZzgH1kf2RwRsiAxNpP_yrpytBbOMIXHUr8pe1QVsOjo5AiyN8QuIpYh1kqQCSuwdKEoXqWucPTIh01mMDbKzvAZmkpZEd440NniDIxO5gw7hJFbogWduld063kM5cnlpoqRiE2sHWwiDstb4XvPPXWW9wwBNaQjznQPWyR1O0UIr3jTA=w805-h803-no"
-                alt="Me"
-                className="profile"
-              />
-            </div>
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseHover = this.handleMouseHover.bind(this);
+    this.state = {
+      isHovering: false
+    };
+  }
+  handleMouseHover() {
+    this.setState(this.toggleHoverState);
+  }
+
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering
+    };
+  }
+
+  render() {
+    return (
+      <div className="contact-container">
+        <div className="container contact-content">
+          <div className="row">
+            <h1 className="contact-title">Let's Connect!</h1>
           </div>
-          <div className="col-sm-6">
-            <div className="row contact-row-2">
-              <div className="col-sm-3">
-                <a href="#">
-                  <img
-                    className="contact-icon"
-                    src="https://lh3.googleusercontent.com/ELhkIS-ikr-bktu-NPrGm350klaaMg_m3PSSI3SWYtkYqzH_UrBr-Gi039GGuYV-fL2Jl8NUEX2mPAG3RCPHvv_sn14J_orc94VBUQYRNe-jE4Tr4aOrfWeNxdNx85bw1vYP4amJEScLdlhz2ONjnV3uXgVyPK9y5bZzDlyRH0sECarhnLwxxwszfWsb2bpWcrx8iAWvqgyxVGBdj-EeQUXVGi3Bsdw7XmdlGqenkU1lxAH0r73-fw2muQG0jNW6vT-LKGsukAMD0Obj3mDTCRB5JC79sz4jRFQnsYHHOCOL52EHEe06ITH1HxKYFRUbHP0dH77J3yC5cXjviEYN-gbbpcut3ueOaenVi-QLV4HldTZxAsfhHI_q8QS0gGI4CHzAlW8oAop8jf62IQxz9-ZtZFFEuc41G0UewYPG4UJM-j7R8R8ITlPb9VJD9oai2eT-PG8P-Gv5ZYC5kha_GS49uwyNYmqambtkW_DqZXQpmoaLzCotW9OtHe6CMzIuO5Z_Fl0ANcZ03CaCWZf0CYOqwH32FC9kFHlaL9DzLfE-CrGyDgDaK4Kmt7WZjlynVFcA1BRHykfsGofM0MNNiitoiTF-VHMuHv6PdA=s256-no"
-                    alt="location"
-                  />
-                </a>
+          <div className="row contact-row">
+            <div className="col-sm-6 column">
+              <div className="me-pic">
+                <img
+                  src="https://lh3.googleusercontent.com/q4RmFnhkbD1uTfkieAhxGWJGLnyikDxKNwvdWSGr-c0gZTDSlBrGQAHUlIGwd4ytkiibuiJZ2XIohk-iOh6XsICUGm5AxzxYwxyQ-eL7pjPmkhAA98OXjMI_OYcE4iQZCvh5FfzkW9AZ3xnH26ebv9Eb2hf9c3QcEqzmkgP_bW1Tue6WzouVkeeOXV5bLZ9zgRqm9WONjP-BsGTZ3OOUjzzSScElcOMInLhq4C3TD50VyDUOhA_KDDiQnaoOoNJzY4q9R5HtrVrlTSLT0u65XZMX3BiAlgDp3UWZoOyeNPPbufUeu4oLr7_I5tB8Ls5MUI6EVKz2ysepAuK6W2ericGbaQMz9zwfr6REurkE84llioxXx1DrcC55qK4ncJ-KUYJ3hR9U_w1OnaQbqDwrD3iMDgTs07Jo6edcZ40Tu9pW1howCES7DWZzgH1kf2RwRsiAxNpP_yrpytBbOMIXHUr8pe1QVsOjo5AiyN8QuIpYh1kqQCSuwdKEoXqWucPTIh01mMDbKzvAZmkpZEd440NniDIxO5gw7hJFbogWduld063kM5cnlpoqRiE2sHWwiDstb4XvPPXWW9wwBNaQjznQPWyR1O0UIr3jTA=w805-h803-no"
+                  alt="Me"
+                  className="profile"
+                />
               </div>
-              <div className="col-sm-3">
-                <a href="#">
-                  <img
-                    className="contact-icon"
-                    src="https://lh3.googleusercontent.com/FFzLvYecvNlUFXsNsfPr1M_AJGLHcvfHJXOnEFQoOHUY6VkK_yiD-eOQF1nHjK5t9sj6_gDIfI8nhsscuDm8GEfNha8X4ZN86ZgVlGZIcYfSnuGgdv5_gU8EczkmK4yl_qiOn4Gzn4DxyMkNpwffQIjWG3YOp4EcmXB3C4Dzwbks8up71cRPm0gSaOUvJ9BVNkWDSiqlaHg9xGlAHCIkgXhJixfEOfB-72tv3brhOP4D3_MRvr4Wgoz57JMFtywE1gfpOUmJSsGaYySTlqNCfD5-fxPyBXlwDiM7w_uFSqS7SD5nDoV_S5c-v0TRm0MISAM46UNjhFvMVm5caYiJH51lQJYgX46Up5ow6E5TvHEh3ODncA1MZcMXa7m2_SRCSEbtFOS50jiPCFuC6T8xO7lhaWfwCVbBs3hKoOl7sucr3LzICrm51gCYMl4nYRI_z4gWmP479smSwmbRkTT7kjAfX5F9BJOtNKlQZXO_Fw5OY1wp3rqRpRkHmlay1xh8_tAxCrF9SFrCUg7HCKsgelzScoHHNM4Gnx62-4skbTVhZXNlyNRJfDOBI6P2nDwQ72mFMLgHJ0LadrxgTCkfpX0Y4_hRGwsh1uh5Jg=s256-no"
-                    alt="email"
-                  />
-                </a>
-              </div>
-              <div className="col-sm-3">
-                <a href="#">
-                  <img
-                    className="contact-icon"
-                    src="https://lh3.googleusercontent.com/gutAZiD4Gq2X4NryvtcZXdbgUj6XsCZ9hH1F4IubMsRh2hagpjfvHEqO6H5YJiPesHCxYjha-dEaulErNmUA3a2IWO4f6mWhBifYJNw5c11VrUHd3dCOtE3InG1EGMnqeLoMMn7xJApX_N8SP9462ArIjVjFHAH_-kuv3ibI_1uIB1E4hur0-mZaHJy6p8GqanPC91T4-zs6DMvysBkEZBJJ9VOf1HLNrohIcpoQvcTBlvRtafR8zYNDllOgtsCPwndGPDJ6uUZhV5-oiYJfP_eNCXOpDvSIUobMUrLn0S21BdHHR9EejCMqrEN-TkPmamaQEdPUNalUITuNCcxGJ6i_qh0esSK7baKxAw3Pe8psNFVy8FT-LKENmjMgbc7OUyrmA2zwNXlk-zt2WDfpWi6if7Nvq9KLenw_2AkuZ-7YwCG9DpgoMm9TuzbvsQ2gV-cQp-zipJFK2Byvhx2S7JKlnRFE0lTh21iSibWDFcFsUe0tb4Bo6JbZAmSvXvjDEVcz9a4H93zZSqfsqJCcEo5iP1cMhdmsI70L7hrrbpHCRuSbhUzTpRmBy1RODvLKxRgpGRqXr9EV-d6_-EOVU8ZQIhsoFlTywS8B0w=s256-no"
-                    alt="github"
-                  />
-                </a>
-              </div>
-              <div className="col-sm-3">
-                <a href="#">
-                  <img
-                    className="contact-icon"
-                    src="https://lh3.googleusercontent.com/l6qWFRvrUxOUpCU8SUEK_B0yLCHur6Gidf0BVTRBS3MF8RV73gjcedOPzrGyeFIPexq9WKVC1QQjeZO577PkGZRUe5Z5vn-FfOFGtnLLTnSWv1BfiREuSmfQJMrWq7F4zMsgeuiA5IUaWi3bQvrfTeO4qwQw5yToQ_rSdNw3XFIbur26Xnm8mlMgqQZECtTzUf6j8dNYKzGDOucHZEiAT7u8HrL5mvb4OoEXsRVLcuTdn8ZXKgjaWRPKJ1AZzePhJEgM1b99w0Jjb1PI_kBPVEyMyZVUXnYg1sxtpnTEwuIcETjg802pD8pSpfqM4VfT89pD_HNags9AOF_JBKIHzSpzcVatwQRZ87O-VpqZ3292f3xgynX7SfUj6Ccop-8yHgGxbJzv1Clutgd7vYGyIARVrX4u6kAUE8GZ3y0TKau5cr0k9BZ7KIpGTu76XOdgpVCZkFRMjAVOVJw00y9V6Yzuxv8UJbALd6UcG6giKNDWrwVqVEpHu6k_r5Gm_-Xh2NeQIV-MOEYCFM6aXHMc6_pXDHEr-3fkcytEQKgEtmLRK26IRal7fKLs2b1RKYxJ6FvIDiXdWO_8b_wI87F0C7nIM6wACFZ9TykSpA=s256-no"
-                    alt="facebook"
-                  />
-                </a>
+            </div>
+            <div className="col-sm-6">
+              <div className="row contact-row-2">
+                <div className="col-sm-3">
+                  <a
+                    href="#"
+                    onMouseEnter={this.handleMouseHover}
+                    onMouseLeave={this.handleMouseHover}
+                  >
+                    <img
+                      className="contact-icon"
+                      src="https://lh3.googleusercontent.com/ELhkIS-ikr-bktu-NPrGm350klaaMg_m3PSSI3SWYtkYqzH_UrBr-Gi039GGuYV-fL2Jl8NUEX2mPAG3RCPHvv_sn14J_orc94VBUQYRNe-jE4Tr4aOrfWeNxdNx85bw1vYP4amJEScLdlhz2ONjnV3uXgVyPK9y5bZzDlyRH0sECarhnLwxxwszfWsb2bpWcrx8iAWvqgyxVGBdj-EeQUXVGi3Bsdw7XmdlGqenkU1lxAH0r73-fw2muQG0jNW6vT-LKGsukAMD0Obj3mDTCRB5JC79sz4jRFQnsYHHOCOL52EHEe06ITH1HxKYFRUbHP0dH77J3yC5cXjviEYN-gbbpcut3ueOaenVi-QLV4HldTZxAsfhHI_q8QS0gGI4CHzAlW8oAop8jf62IQxz9-ZtZFFEuc41G0UewYPG4UJM-j7R8R8ITlPb9VJD9oai2eT-PG8P-Gv5ZYC5kha_GS49uwyNYmqambtkW_DqZXQpmoaLzCotW9OtHe6CMzIuO5Z_Fl0ANcZ03CaCWZf0CYOqwH32FC9kFHlaL9DzLfE-CrGyDgDaK4Kmt7WZjlynVFcA1BRHykfsGofM0MNNiitoiTF-VHMuHv6PdA=s256-no"
+                      alt="location"
+                    />
+                  </a>
+
+                  {this.state.isHovering && (
+                    <div className="hovering-div">
+                      <p className="location">Newport Beach, CA</p>
+                      <p className="location-2">
+                        Interested in remote and relocation opportunities!
+                      </p>
+                    </div>
+                  )}
+                </div>
+                <div className="col-sm-3">
+                  <a href="#">
+                    <img
+                      className="contact-icon"
+                      src="https://lh3.googleusercontent.com/FFzLvYecvNlUFXsNsfPr1M_AJGLHcvfHJXOnEFQoOHUY6VkK_yiD-eOQF1nHjK5t9sj6_gDIfI8nhsscuDm8GEfNha8X4ZN86ZgVlGZIcYfSnuGgdv5_gU8EczkmK4yl_qiOn4Gzn4DxyMkNpwffQIjWG3YOp4EcmXB3C4Dzwbks8up71cRPm0gSaOUvJ9BVNkWDSiqlaHg9xGlAHCIkgXhJixfEOfB-72tv3brhOP4D3_MRvr4Wgoz57JMFtywE1gfpOUmJSsGaYySTlqNCfD5-fxPyBXlwDiM7w_uFSqS7SD5nDoV_S5c-v0TRm0MISAM46UNjhFvMVm5caYiJH51lQJYgX46Up5ow6E5TvHEh3ODncA1MZcMXa7m2_SRCSEbtFOS50jiPCFuC6T8xO7lhaWfwCVbBs3hKoOl7sucr3LzICrm51gCYMl4nYRI_z4gWmP479smSwmbRkTT7kjAfX5F9BJOtNKlQZXO_Fw5OY1wp3rqRpRkHmlay1xh8_tAxCrF9SFrCUg7HCKsgelzScoHHNM4Gnx62-4skbTVhZXNlyNRJfDOBI6P2nDwQ72mFMLgHJ0LadrxgTCkfpX0Y4_hRGwsh1uh5Jg=s256-no"
+                      alt="email"
+                    />
+                  </a>
+                </div>
+                <div className="col-sm-3">
+                  <a href="#">
+                    <img
+                      className="contact-icon"
+                      src="https://lh3.googleusercontent.com/gutAZiD4Gq2X4NryvtcZXdbgUj6XsCZ9hH1F4IubMsRh2hagpjfvHEqO6H5YJiPesHCxYjha-dEaulErNmUA3a2IWO4f6mWhBifYJNw5c11VrUHd3dCOtE3InG1EGMnqeLoMMn7xJApX_N8SP9462ArIjVjFHAH_-kuv3ibI_1uIB1E4hur0-mZaHJy6p8GqanPC91T4-zs6DMvysBkEZBJJ9VOf1HLNrohIcpoQvcTBlvRtafR8zYNDllOgtsCPwndGPDJ6uUZhV5-oiYJfP_eNCXOpDvSIUobMUrLn0S21BdHHR9EejCMqrEN-TkPmamaQEdPUNalUITuNCcxGJ6i_qh0esSK7baKxAw3Pe8psNFVy8FT-LKENmjMgbc7OUyrmA2zwNXlk-zt2WDfpWi6if7Nvq9KLenw_2AkuZ-7YwCG9DpgoMm9TuzbvsQ2gV-cQp-zipJFK2Byvhx2S7JKlnRFE0lTh21iSibWDFcFsUe0tb4Bo6JbZAmSvXvjDEVcz9a4H93zZSqfsqJCcEo5iP1cMhdmsI70L7hrrbpHCRuSbhUzTpRmBy1RODvLKxRgpGRqXr9EV-d6_-EOVU8ZQIhsoFlTywS8B0w=s256-no"
+                      alt="github"
+                    />
+                  </a>
+                </div>
+                <div className="col-sm-3">
+                  <a href="#">
+                    <img
+                      className="contact-icon"
+                      src="https://lh3.googleusercontent.com/l6qWFRvrUxOUpCU8SUEK_B0yLCHur6Gidf0BVTRBS3MF8RV73gjcedOPzrGyeFIPexq9WKVC1QQjeZO577PkGZRUe5Z5vn-FfOFGtnLLTnSWv1BfiREuSmfQJMrWq7F4zMsgeuiA5IUaWi3bQvrfTeO4qwQw5yToQ_rSdNw3XFIbur26Xnm8mlMgqQZECtTzUf6j8dNYKzGDOucHZEiAT7u8HrL5mvb4OoEXsRVLcuTdn8ZXKgjaWRPKJ1AZzePhJEgM1b99w0Jjb1PI_kBPVEyMyZVUXnYg1sxtpnTEwuIcETjg802pD8pSpfqM4VfT89pD_HNags9AOF_JBKIHzSpzcVatwQRZ87O-VpqZ3292f3xgynX7SfUj6Ccop-8yHgGxbJzv1Clutgd7vYGyIARVrX4u6kAUE8GZ3y0TKau5cr0k9BZ7KIpGTu76XOdgpVCZkFRMjAVOVJw00y9V6Yzuxv8UJbALd6UcG6giKNDWrwVqVEpHu6k_r5Gm_-Xh2NeQIV-MOEYCFM6aXHMc6_pXDHEr-3fkcytEQKgEtmLRK26IRal7fKLs2b1RKYxJ6FvIDiXdWO_8b_wI87F0C7nIM6wACFZ9TykSpA=s256-no"
+                      alt="facebook"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Contact;
