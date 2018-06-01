f0f74980cdf8446c42a88db0fc57477f33ec6605
Rebecca A. Olson <rebeccaolson13@gmail.com>
2018-05-31 22:06:07 -0700

    scroll to top button working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,7 @@ import Experience from '../Experience';
 import Contact from '../Contact';
 import Tech from '../Technologies';
 import Footer from '../Footer';
+import ScrollUpButton from 'react-scroll-up-button';
 import './App.css';
 
 class App extends Component {
@@ -14,8 +15,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      ScrollUpButton: {
+        color: '#ffffff'
+      }
+    };
     return (
       <div className="App">
+        <ScrollUpButton
+          ContainerClassName="ScrollUpButton__Container"
+          TransitionClassName="ScrollUpButton__Toggled"
+          AnimationDuration={450}
+          style={style.ScrollUpButton}
+        />
+
         <Header />
 
         <Branding />
